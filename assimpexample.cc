fe8254930684cd4412cc721cf5fec1514cdae167
Jaewon Choi <jaewon.james.choi@gmail.com>
2017-12-26 01:16:30 +0900

    Add index buffers

--- assimpexample.cc
+++ assimpexample.cc
@@ -148,6 +148,15 @@ int main()
     glBindBuffer(GL_ARRAY_BUFFER, vertexBufferID);
     glBufferData(GL_ARRAY_BUFFER, sizeof(float) * vertices.size(), vertices.data(), GL_STATIC_DRAW);
 
+    std::vector<uint16_t> indices = {
+        0, 1, 2
+    };
+
+    GLuint indexBufferID;
+    glGenBuffers(1, &indexBufferID);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBufferID);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(uint16_t) * indices.size(), indices.data(), GL_STATIC_DRAW);
+
     glUseProgram(programID);
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void *)0);
@@ -166,7 +175,7 @@ int main()
 
         glEnableVertexAttribArray(0);
 
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_SHORT, nullptr);
 
         glDisableVertexAttribArray(0);
 
@@ -181,6 +190,7 @@ int main()
 
     glDeleteVertexArrays(1, &vertexArrayID);
     glDeleteBuffers(1, &vertexBufferID);
+    glDeleteBuffers(1, &indexBufferID);
 
     glDeleteProgram(programID);
 
