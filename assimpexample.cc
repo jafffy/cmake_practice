d12843e95b3a03fd244f7f5194083136f5d934a0
Jaewon Choi <jaewon.james.choi@gmail.com>
2017-12-26 04:22:57 +0900

    Fix for windows compatibility

--- assimpexample.cc
+++ assimpexample.cc
@@ -114,9 +114,8 @@ int main()
 
     glfwWindowHint(GLFW_SAMPLES, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     window = glfwCreateWindow(800, 600, "Hello World", nullptr, nullptr);
     if (!window)
